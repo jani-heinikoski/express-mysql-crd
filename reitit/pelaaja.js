0034ad372c3f755e8fddd1d2df304db63a2073e5
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-04-02 14:13:17 +0300

    Lisätty kommentteja, HTTP arvoja korjattu, delete reitti korjattu

--- reitit/pelaaja.js
+++ reitit/pelaaja.js
@@ -6,11 +6,19 @@
  * https://expressjs.com/
  * https://www.npmjs.com/package/mysql2
  */
-module.exports = function (mysqlConnection) {
+// Luo pelaaja reittiä varten alustetun Router-objektin instanssin,
+// ja palauttaa referenssin siihen.
+function routerFactory(mysqlConnection) {
+    // Luodaan alustamaton Router-objekti. Käytetään
+    // uusien reittien rekisteröimiseen expressin kanssa.
     const router = require("express").Router();
-    // GET reitti päätepisteeseen /, käytetään pelaajien hakemiseen
+    // Reitit lisätään yksinkertaisimmillaan seuraavasti
+    // router.<http-metodi>(<päätepiste>, <callback funktio>).
+    // Tässä esimerkiksi luodaan GET reitti päätepisteeseen /,
+    // sitä käytetään pelaajien hakemiseen palvelimelta.
     router.get("/", async (req, res) => {
         try {
+            // Haetaan kaikki pelaajat tietokannasta
             const [pelaajat, sarakeMaaritelmat] = await mysqlConnection.execute(
                 "SELECT * FROM PELAAJA"
             );
@@ -19,38 +27,44 @@ module.exports = function (mysqlConnection) {
                 return res.status(200).json({ pelaajat: pelaajat });
             }
         } catch (err) {
+            // Tulostetaan virhe ja lähetetään takaisin clientille HTTP 500
+            // - Internal Server Error statuskoodi, mikäli menee jotain vikaan.
+            console.error(err);
             return res.sendStatus(500);
         }
-        // Ei löytynyt pelaajia -> vastataan HTTP statuskoodilla 404 - Not Found
-        return res.sendStatus(404);
+        // Ei löytynyt pelaajia, lähetetään HTTP 404 takaisin clientille
+        return res.status(200).json({ pelaajat: [] });
     });
     // POST reitti päätepisteeseen /, käytetään pelaajien lisäämiseen
     router.post("/", async (req, res) => {
         const LISAA_PELAAJA_QUERY = `INSERT INTO PELAAJA
         (PELAAJA_NIMI, PELAAJA_SEURA) VALUES
         (?, ?);`;
+        // Jos HTTP pyynnöstä löytyy bodystä lisättävä pelaaja
         if (req.body.pelaaja) {
             try {
                 // Yritetään lisätä pelaaja tietokantaan
-                await mysqlConnection.execute(LISAA_PELAAJA_QUERY, [
-                    req.body.pelaaja.PELAAJA_NIMI,
-                    req.body.pelaaja.PELAAJA_SEURA,
-                ]);
+                console.log(
+                    await mysqlConnection.execute(LISAA_PELAAJA_QUERY, [
+                        req.body.pelaaja.PELAAJA_NIMI,
+                        req.body.pelaaja.PELAAJA_SEURA,
+                    ])
+                );
             } catch (err) {
+                // Tulostetaan virhe ja lähetetään takaisin clientille HTTP 500
+                // - Internal Server Error statuskoodi, mikäli menee jotain vikaan.
                 console.error(err);
                 return res.sendStatus(500);
             }
-            // Lisäys onnistui, lähetetään HTTP 201 takaisin clientille
-            return res.sendStatus(201);
+            // Lisäys onnistui, lähetetään HTTP 204 takaisin clientille
+            return res.sendStatus(204);
         }
-        // Lisäyspyyntö ei sis. pelaajaa -> vastataan HTTP statuskoodilla 400 - Bad request
+        // Lisäyspyyntö ei sis. pelaajaa, lähetetään HTTP 400 takaisin clientille
         return res.sendStatus(400);
     });
     // DELETE reitti päätepisteeseen /, käytetään pelaajien poistamiseen
     router.delete("/", async (req, res) => {
-        const POISTA_PELAAJAT_QUERY = `INSERT INTO PELAAJA
-        (PELAAJA_NIMI, PELAAJA_SEURA) VALUES
-        (?, ?);`;
+        const POISTA_PELAAJAT_QUERY = `DELETE * FROM Pelaaja;`;
         try {
             // Yritetään poistaa pelaajat tietokannasta
             await mysqlConnection.execute(POISTA_PELAAJAT_QUERY);
@@ -60,8 +74,10 @@ module.exports = function (mysqlConnection) {
             console.error(err);
             return res.sendStatus(500);
         }
-        // Lisäys onnistui, lähetetään HTTP 204 takaisin clientille
+        // Poisto onnistui, lähetetään HTTP 204 takaisin clientille
         return res.sendStatus(204);
     });
     return router;
-};
+}
+
+module.exports = routerFactory;
