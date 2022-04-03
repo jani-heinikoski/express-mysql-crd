570f8e828be13aa44c1a8d67709e03363d24efe5
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2022-04-03 17:39:22 +0300

    Kommentteja korjattu, delete SQL query korjattu, luettavuutta lisätty

--- reitit/pelaaja.js
+++ reitit/pelaaja.js
@@ -6,6 +6,25 @@
  * https://expressjs.com/
  * https://www.npmjs.com/package/mysql2
  */
+
+/*
+Tässä listaus, mitä eri HTTP-protokollan statuskoodit tarkoittavat:
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+200 - OK, Pyyntö onnistui
+------------------------------------------------------------------------
+204 - No Content, Pyyntö onnistui, mutta ei ole mitään takaisinlähetettävää.
+Käytetään tässä ilmaisemaan, että uusi resurssi on luotu tai resursseja on poistettu.
+Monesti luotaessa uusia resursseja, joita clientillä ei ole jo hallussaan,
+käytetään koodia 201 - Created, ja lähetetään vastauksen body:ssa luotu
+resurssi takaisin clientille.
+------------------------------------------------------------------------
+500 - Internal Server Error, lähetetään kun palvelimen päässä tapahtuu jokin virhe.
+Huom. useasti on järkevää olla lähettämättä clientille itse virheviestiä, koska
+se voi sisältää informaatiota, jota pahatahtoiset käyttäjät voivat hyödyntää.
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+*/
 // Luo pelaaja reittiä varten alustetun Router-objektin instanssin,
 // ja palauttaa referenssin siihen.
 function routerFactory(mysqlConnection) {
@@ -22,18 +41,18 @@ function routerFactory(mysqlConnection) {
             const [pelaajat, sarakeMaaritelmat] = await mysqlConnection.execute(
                 "SELECT * FROM PELAAJA"
             );
-            if (pelaajat instanceof Array && pelaajat.length > 1) {
+            if (pelaajat instanceof Array && pelaajat.length > 0) {
                 // Löytyi pelaajat, lähetetään takaisin JSON -muodossa
                 return res.status(200).json({ pelaajat: pelaajat });
+            } else {
+                // Ei löytynyt pelaajia, lähetetään tyhjä array takaisin
+                return res.status(200).json({ pelaajat: [] });
             }
         } catch (err) {
-            // Tulostetaan virhe ja lähetetään takaisin clientille HTTP 500
-            // - Internal Server Error statuskoodi, mikäli menee jotain vikaan.
+            // Tulostetaan virhe ja lähetetään takaisin clientille HTTP 500.
             console.error(err);
             return res.sendStatus(500);
         }
-        // Ei löytynyt pelaajia, lähetetään HTTP 404 takaisin clientille
-        return res.status(200).json({ pelaajat: [] });
     });
     // POST reitti päätepisteeseen /, käytetään pelaajien lisäämiseen
     router.post("/", async (req, res) => {
@@ -44,15 +63,12 @@ function routerFactory(mysqlConnection) {
         if (req.body.pelaaja) {
             try {
                 // Yritetään lisätä pelaaja tietokantaan
-                console.log(
-                    await mysqlConnection.execute(LISAA_PELAAJA_QUERY, [
-                        req.body.pelaaja.PELAAJA_NIMI,
-                        req.body.pelaaja.PELAAJA_SEURA,
-                    ])
-                );
+                await mysqlConnection.execute(LISAA_PELAAJA_QUERY, [
+                    req.body.pelaaja.PELAAJA_NIMI,
+                    req.body.pelaaja.PELAAJA_SEURA,
+                ]);
             } catch (err) {
                 // Tulostetaan virhe ja lähetetään takaisin clientille HTTP 500
-                // - Internal Server Error statuskoodi, mikäli menee jotain vikaan.
                 console.error(err);
                 return res.sendStatus(500);
             }
@@ -64,13 +80,12 @@ function routerFactory(mysqlConnection) {
     });
     // DELETE reitti päätepisteeseen /, käytetään pelaajien poistamiseen
     router.delete("/", async (req, res) => {
-        const POISTA_PELAAJAT_QUERY = `DELETE * FROM Pelaaja;`;
+        const POISTA_PELAAJAT_QUERY = `DELETE FROM PELAAJA;`;
         try {
             // Yritetään poistaa pelaajat tietokannasta
             await mysqlConnection.execute(POISTA_PELAAJAT_QUERY);
         } catch (err) {
             // Tulostetaan virhe ja lähetetään takaisin clientille HTTP 500
-            // - Internal Server Error statuskoodi, mikäli menee jotain vikaan.
             console.error(err);
             return res.sendStatus(500);
         }
